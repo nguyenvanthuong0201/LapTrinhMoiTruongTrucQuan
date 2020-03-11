45c6c1b21e661ddd9a775a408b344a01189cd0a0
nguyenvanthuong0201 <55219637+nguyenvanthuong0201@users.noreply.github.com>
2020-03-11 12:42:50 +0700

    TinhDienTich

---
+++ Module1/TinhDienTich/TinhDienTich/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinhDienTich
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dai, rong, dientich=0, chuvi = 0;
+            Console.Clear();
+            Console.WriteLine("********** TinhDienTich va ChuVI **********");
+            Console.Write("nhap so thu 1: ");
+            dai = Convert.ToInt32(Console.ReadLine());
+            Console.Write("nhap so thu 2: ");
+            rong = Convert.ToInt32(Console.ReadLine());
+            dientich = dai * rong;
+            chuvi = (dai + rong) * 2;
+            Console.WriteLine("Chuvi: " + chuvi);
+            Console.WriteLine("DienTich: " + dientich);
+            Console.ReadLine();
+        }
+    }
+}
