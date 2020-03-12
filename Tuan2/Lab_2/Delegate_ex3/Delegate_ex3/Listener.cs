11a36ed8704cc19ebd58deb88ec47a299af85b34
nguyenvanthuong0201 <55219637+nguyenvanthuong0201@users.noreply.github.com>
2020-03-12 22:19:12 +0700

    Lab-2

---
+++ Tuan2/Lab_2/Delegate_ex3/Delegate_ex3/Listener.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_ex3
+{
+    class Listener
+    {
+        public void GetNotified(string sInfo)
+        {
+            Console.WriteLine("I got notified with the following information {0}", sInfo);
+        }
+        public static void GetNotifiedAgain(string sInfo)
+        {
+            Console.WriteLine("I got notified with the following information:{0}", sInfo);
+        }
+        static void InvokeDelegate(NotifyMe d)
+        {
+            d("You are late paying your bills!");
+        }
+    }
+}
